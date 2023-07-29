ed0c49559b94df207d57fc24a60aebac5b15367e
Sujith Sojan <sujithsojan@gmail.com>
2023-07-29 11:48:44 +0530

    Issue #4 fix: Add a loading for waiting time

---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.css']
+})
+export class PersonsComponent implements OnInit,OnDestroy {
+
+  personsList: string[]= [];
+  isFetched = false;
+  private personListSubs: Subscription = new Subscription();
+  constructor(private prsService: PersonsService) {
+   }
+
+   ngOnInit() {
+     this.personListSubs= this.prsService.personChanged.subscribe(persons => {
+      this.personsList = persons;
+      this.isFetched = false;
+     });
+     this.isFetched = true;
+     this.prsService.fetchPersons();
+   }
+
+   ngOnDestroy() {
+    this.personListSubs.unsubscribe();
+   }
+
+   onPersonRemove(personName: string) {
+    this.prsService.removePerson(personName);
+   }
+
+
+
+
+}
